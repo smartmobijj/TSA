8ec22d2d6166cd8b602ed4e100a6b8a706f1a068
smartmobijj <121627763+smartmobijj@users.noreply.github.com>
2024-03-12 01:57:02 -0400

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -6,7 +6,7 @@ workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
 
 threads = int(os.environ.get('GUNICORN_THREADS', '2'))
 
-# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
 
